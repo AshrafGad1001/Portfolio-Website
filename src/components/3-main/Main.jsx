076ecc322d7eb308bfa18562bbe254c3411e59c6
AshrafGad1001 <ashrafgad542@gmail.com>
2026-04-16 06:40:29 +0200

    main components

---
+++ src/components/3-main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './main.css';
+
+const main = () => {
+    return (
+        <div>
+            main
+        </div>
+    );
+}
+
+
+
+
+export default main;
---
+++ src/components/4-contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './contact.css';
+
+const contact = () => {
+    return (
+        <div>
+            contact
+        </div>
+    );
+}
+
+
+
+
+export default contact;
