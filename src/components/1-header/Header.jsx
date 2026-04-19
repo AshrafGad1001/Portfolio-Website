830aeaff6a07486b53c65e52b4bff02da7775101
AshrafGad1001 <ashrafgad542@gmail.com>
2026-04-19 08:08:11 +0200

    Complete Header

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import './header.css';
 
 const Header = () => {
+
+
+    const [showpopup, setshowpopup] = useState(false);
+
+
+
     return (
-        <header className="border flex">
+        <header className="flex">
+            <button className="menu icon-menu" onClick={() => {
+                setshowpopup(true);
+            }}>
+            </button>
+            <div />
             <nav>
                 <ul className="flex">
                     <li><a href="">About</a></li>
@@ -14,9 +26,30 @@ const Header = () => {
                 </ul>
             </nav>
 
-            <button>
-                light
+            <button className="themes">
+                <span className="icon-weather-sunny"></span>
+                {/* <span className="icon-moon-o"></span> */}
             </button>
+
+
+            {
+                showpopup && (<div className="fixed">
+                    <ul className="pop-up">
+                        <li>
+                            <button className="icon-times" onClick={() => {
+                                setshowpopup(false);
+                            }}>
+                            </button>
+                        </li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Article</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Speaking</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+
+                </div>)
+            }
         </header>
     );
 }
