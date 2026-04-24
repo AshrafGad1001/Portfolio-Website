9c49aa5c3a558d4073b7c91f6c3b42ce638a73b4
AshrafGad1001 <ashrafgad542@gmail.com>
2026-04-24 06:13:37 +0300

    contact-Us

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,11 +1,44 @@
-import React from "react";
 import './contact.css';
 
 const contact = () => {
     return (
-        <div>
-            contact
-        </div>
+        <section className='contact-us'>
+
+            <h2 className='title'>
+                <span className='icon-envelope'></span>
+                Contact Us
+            </h2>
+
+            <p className='sub-title'>Contact Us For More Information and Get notefied when i publish Something New</p>
+
+
+
+            <div className="flex">
+                <form >
+                    <div className='form-group'>
+                        <label htmlFor="email">Email Address : </label>
+                        <input required placeholder='Enter Your Email' type="email" name="email" id="email" />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor="message">Your Message : </label>
+                        <textarea required placeholder='Enter Your message' name="message" id="message"></textarea>
+                    </div>
+
+                    <button className='submit' >Submit</button>
+                </form>
+
+
+
+
+
+                <div className="animation border">animation</div>
+            </div>
+
+
+
+
+        </section>
     );
 }
 
